36ffeea5f493229f626499f1a843d53c8520bff6
VictoriaKonuhova <konyukhova.victoria@gmail.com>
2023-04-14 16:14:26 +0300

    section photo-gallery

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,18 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-left',
   },
 });
+
+const swiperG = new Swiper('.swiper__g', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: "fraction",
+  },
+
+});
